e6c5934a9f82d0a8863ab4ed63157d52196be359
Chanthima Pinkaew <chunthima15@gmail.com>
2020-07-04 17:29:47 +0700

    3.2 done

---
+++ 3.2/3.2.ino
@@ -0,0 +1,18 @@
+const int buzzer = 8;
+
+void setup() {
+  pinMode(buzzer, OUTPUT);
+  tone(buzzer, 400);
+  delay(400);
+  noTone(buzzer);
+  delay(200);
+  tone(buzzer, 400);
+  delay(400);
+  noTone(buzzer);
+  delay(200);
+  tone(buzzer, 200);
+  delay(200);
+  noTone(buzzer);
+}
+
+void loop() {}
